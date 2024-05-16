789469639a29982d4149de005abd9314b555a9f3
hfsasu <hafsaasude2@gmail.com>
2024-05-16 18:11:37 -0500

    all the new updates

---
+++ Project 2/hell.py
@@ -0,0 +1,19 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+def main():
+    try:
+        num = int(input("Enter a non-negative integer: "))
+        if num < 0:
+            print("Factorial is not defined for negative numbers.")
+        else:
+            result = factorial(num)
+            print(f"The factorial of {num} is {result}.")
+    except ValueError:
+        print("Invalid input. Please enter a valid integer.")
+
+if __name__ == "__main__":
+    main()
